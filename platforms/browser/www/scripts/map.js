c5dced78d245d4e3c8f2c9a003f8ca99db3a5651
X Coan <xjcoan@gmail.com>
2017-11-14 23:50:13 -0500

    Update map w/ more lots and ajax

--- platforms/browser/www/scripts/map.js
+++ platforms/browser/www/scripts/map.js
@@ -1,43 +1,74 @@
-var lots = [
-  {'lot': 'Ross Ade', 'latlng': '40.436993,-86.918943'},
-  {'lot': 'Cary Quad', 'latlng': '40.433064,-86.917838'},
-  {'lot': 'Francis A. Cordova', 'latlng': '40.429502,-86.922777'}
-];
+$(document).ready(function() {
+  // Simulated data that would come from parking sensors/web server
+  var lots;
 
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.428558, lng: -86.9136493},
-    zoom: 14
-  });
+  var jqxhr = $.getJSON( "scripts/data.json", function() {})
+    .done(function(json) {
+      lots = json;
+      var myLatLng = new google.maps.LatLng( 40.428558, -86.9136493);
 
-  // loop through lots and place markers w/ cards
-  for (var lot in lots) {
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h2 id="firstHeading" class="firstHeading">'+ lots[lot]['lot'] + '</h2>'+
-      '<div id="bodyContent">'+
-      '<p>3/50 Spaces Available</p>' +
-      '</div>'+
-      '</div>';
+      var MapSettings ={
+        zoom: 14,
+        center: myLatLng,
+        mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.BOTTOM_CENTER
+          },
+          fullscreenControl: true,
+          fullscreenControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER
+          },
+          zoomControl: true,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+          },
+          scaleControl: true,
+          streetViewControl: false,
+          streetViewControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER
+          }
+      }
+      var map = new google.maps.Map(document.getElementById('map'), MapSettings);
+      google.maps.event.addDomListener(window, 'resize', function() {
+          map.setCenter(MapSettings.center);
+      });
+
+      function addMarker(lot) {
+        var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h2 id="firstHeading" class="firstHeading">'+lot['lot']+'</h2>'+
+          '<div id="bodyContent">'+
+          '<p>'+lot['spots']+' spaces available</p>'+
+          '</div>'+
+          '</div>';
+
+        var coords = lot['latlng'].split(',');
+        var marker = new google.maps.Marker({
+          position: {lat: parseFloat(coords[0]), lng: parseFloat(coords[1])},
+          map: map,
+          title: lot['lot'],
+          contentString: contentString
+        });
 
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        marker.addListener('click', function() {
+          infowindow.setContent(this.contentString);
+          infowindow.open(map, this);
+        });
+      }
+
+      for (lot in lots) {
+        addMarker(lots[lot]);
+      }
+    })
+    .fail(function() {
+      console.log( "error" );
     });
 
-    var coords = lots[lot]['latlng'].split(',');
-    console.log(coords);
-
-    var marker = new google.maps.Marker({
-      position: {lat: parseFloat(coords[0]), lng: parseFloat(coords[1])},
-      map: map,
-      title: lots[lot]['lot']
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  }
-
-}
+});
