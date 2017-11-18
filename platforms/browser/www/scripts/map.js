85bada91555a1d8bef77abad82501488db2b80b3
X Coan <xjcoan@gmail.com>
2017-11-17 19:08:18 -0500

    Fix map load issue.

--- platforms/browser/www/scripts/map.js
+++ platforms/browser/www/scripts/map.js
@@ -1,40 +1,36 @@
-$(document).ready(function() {
-  // Simulated data that would come from parking sensors/web server
-  var lots;
+function initMap() {
+  // Initialize Map settings
+  var myLatLng = new google.maps.LatLng(40.428558, -86.9136493);
+  var MapSettings ={
+    zoom: 15,
+    center: myLatLng,
+    mapTypeControl: false,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.BOTTOM_CENTER
+      },
+      fullscreenControl: false,
+      fullscreenControlOptions: {
+        position: google.maps.ControlPosition.BOTTOM_CENTER
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      },
+      scaleControl: false,
+      streetViewControl: false,
+      streetViewControlOptions: {
+        position: google.maps.ControlPosition.BOTTOM_CENTER
+      }
+  }
+  // Create map object with settings
+  var map = new google.maps.Map(document.getElementById('map'), MapSettings);
 
+  //Simulated data that would come from parking sensors/web server
   var jqxhr = $.getJSON( "scripts/data.json", function() {})
     .done(function(json) {
-      lots = json;
-      var myLatLng = new google.maps.LatLng(40.428558, -86.9136493);
-
-      var MapSettings ={
-        zoom: 15,
-        center: myLatLng,
-        mapTypeControl: false,
-          mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            position: google.maps.ControlPosition.BOTTOM_CENTER
-          },
-          fullscreenControl: false,
-          fullscreenControlOptions: {
-            position: google.maps.ControlPosition.BOTTOM_CENTER
-          },
-          zoomControl: true,
-          zoomControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_CENTER
-          },
-          scaleControl: false,
-          streetViewControl: false,
-          streetViewControlOptions: {
-            position: google.maps.ControlPosition.BOTTOM_CENTER
-          }
-      }
-      var map = new google.maps.Map(document.getElementById('map'), MapSettings);
-      google.maps.event.addDomListener(window, 'resize', function() {
-          map.setCenter(MapSettings.center);
-      });
-
-      function addMarker(lot) {
+      // place marker on page
+      function placeMarker(lot) {
         var markerColor = "blue";
         if (lot['spots'].charAt(0) == '0') {
           markerColor = "red"; // lot is full
@@ -57,7 +53,6 @@ $(document).ready(function() {
           contentString: contentString
         });
 
-
         var infowindow = new google.maps.InfoWindow({
           content: contentString
         });
@@ -67,12 +62,13 @@ $(document).ready(function() {
           infowindow.open(map, this);
         });
       }
-
-      for (lot in lots) {
-        addMarker(lots[lot]);
+      // Place markers on map for each parking lot
+      for (lot in json) {
+        placeMarker(json[lot]);
       }
     })
     .fail(function() {
-      alert('Something went wrong');
+      var newDiv = "<div><h1>Something went wrong. Please contact support.</h2></div>";
+      $("#map").append(newDiv);
     });
-});
+}
